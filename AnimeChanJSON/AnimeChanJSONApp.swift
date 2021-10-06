2abb8421a26df1dcae39452253f8bf4e373cf66d
jina <psalvarajah@lcs.on.ca>
2021-10-06 10:46:11 -0400

    Initial Commit

---
+++ AnimeChanJSON/AnimeChanJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeChanJSONApp.swift
+//  AnimeChanJSON
+//
+//  Created by Salvarajah, Prajina on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct AnimeChanJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeChanJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimeChanJSON
+//
+//  Created by Salvarajah, Prajina on 2021-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
